b56361034276d69287356f0ad7e8f633dc21daee
Michael <osujim2012@gmail.com>
2021-11-19 09:21:53 -0500

    Add class that handles the different algorithm choices.

---
+++ js/classes/PathAlgorithmSelect.js
@@ -0,0 +1,38 @@
+class PathAlgorithmSelect {
+  constructor(algorithms) {
+    this.currentSelection = algorithms[0].function;
+    this.map = new WeakMap();
+
+    const pathfindingChoicesElement = document.createElement('div');
+    pathfindingChoicesElement.className = 'path-choices';
+
+    for (let algorithm of algorithms) {
+      const pathfindingAlgorithmElement = document.createElement('div');
+      this.map.set(algorithm.function, pathfindingAlgorithmElement);
+      pathfindingAlgorithmElement.innerHTML = algorithm.name;
+      pathfindingAlgorithmElement.className =
+        'path-choice' +
+        (algorithm.function === this.currentSelection ? ' selected' : '');
+      pathfindingAlgorithmElement.addEventListener(
+        'click',
+        this.select.bind(this, algorithm.function)
+      );
+      pathfindingChoicesElement.appendChild(pathfindingAlgorithmElement);
+    }
+    this.DOMElement = pathfindingChoicesElement;
+  }
+
+  select(algorithm) {
+    const element = this.map.get(algorithm);
+    const previousElement = this.map.get(this.currentSelection);
+    console.log(this.map);
+    console.log(element);
+    console.log(previousElement);
+
+    this.currentSelection = algorithm;
+    element.className = 'path-choice selected';
+    previousElement.className = 'path-choice';
+  }
+}
+
+export default PathAlgorithmSelect;
