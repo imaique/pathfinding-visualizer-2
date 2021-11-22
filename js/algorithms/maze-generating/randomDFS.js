409d5d04a26e2ff2e0b4ca49a16a9498e7505ea8
Michael <osujim2012@gmail.com>
2021-11-22 09:56:10 -0500

    Add random-dfs as maze generating algorithm

--- js/algorithms/maze-generating/randomDFS.js
+++ js/algorithms/maze-generating/randomDFS.js
@@ -1,12 +1,36 @@
-import Stack from "../../data_structures/Stack"
-import { getNeighborIncrements } from "../pathfinding/shared";
+import Stack from '../../data_structures/Stack.js';
+import NodeStates from '../../enums/NodeStates.js';
+import { getNeighborIncrements, isValid, shuffle } from '../shared.js';
 
 export const randomDFS = (start, end, grid) => {
-    const neighbors = getNeighborIncrements(false);
-    const stack = new Stack();
-    stack.push(start);
-    while(!stack.isEmpty()) {
-        
-    }
+  const neighbors = getNeighborIncrements(false);
+  const stack = new Stack();
+  const visited = new Set();
+  const visitedOrder = [];
 
-}
+  stack.push(start);
+
+  while (!stack.isEmpty()) {
+    const current = stack.pop();
+    if (visited.has(`${current.y}_${current.x}`)) continue;
+    visited.add(`${current.y}_${current.x}`);
+    if (current.wall !== undefined) visitedOrder.push(current.wall);
+    visitedOrder.push(current);
+    let possibilities = [];
+    for (let increments of neighbors) {
+      const neighbor = {
+        x: current.x + increments.x * 2,
+        y: current.y + increments.y * 2,
+        wall: { x: current.x + increments.x, y: current.y + increments.y },
+      };
+      if (isValid(neighbor.x, neighbor.y, grid, visited, NodeStates.unvisited))
+        possibilities.push(neighbor);
+    }
+    possibilities = shuffle(possibilities);
+    for (let neighbor of possibilities) {
+      stack.push(neighbor);
+    }
+  }
+
+  return visitedOrder;
+};
